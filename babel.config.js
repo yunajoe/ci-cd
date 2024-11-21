24f9ef0285c31d43ac0e158a81c0cd6554a63ef7
yunajoe <yunajoe@gmail.com>
2024-11-21 15:50:28 +0900

    barbel 재구성

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   presets: [
-    ["@babel/preset-env", { targets: { node: "current" } }],
-    "@babel/preset-typescript",
+    "@babel/preset-env", // 최신 JS 문법을 구 버전의 JS로 변환
+    "@babel/preset-react", // React JSX 변환
+  ],
+  plugins: [
+    "@babel/plugin-transform-modules-commonjs", // ESM -> CommonJS 변환
   ],
 };
