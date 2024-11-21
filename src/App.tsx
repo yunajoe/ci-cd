3e1b4b6be05bc14448d8cb31fbce9b029e8a0b5a
yunajoe <yunajoe@gmail.com>
2024-11-21 14:36:56 +0900

    test작성안하고 push해보겠음

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const handleIncrease = () => {
+    setCount((prev) => prev + 1);
+  };
+  const handleDecrease = () => {
+    setCount((prev) => prev - 1);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>{count}</h1>
+      <button onClick={handleIncrease}>증가하기</button>
+      <button onClick={handleDecrease}>감소하기</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
