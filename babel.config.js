5da98f21cf9c14f5a72b014610f85c291bcd1265
yunajoe <yunajoe@gmail.com>
2024-11-21 16:05:20 +0900

    babel config파일명변경

--- babel.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  presets: [
-    "@babel/preset-env", // 최신 JS 문법을 구 버전의 JS로 변환
-    "@babel/preset-react", // React JSX 변환
-  ],
-  plugins: [
-    "@babel/plugin-transform-modules-commonjs", // ESM -> CommonJS 변환
-  ],
-};
